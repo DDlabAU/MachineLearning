3fd96d651f658aba57e51120b008136d21cf9a23
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-05-13 16:44:25 +0200

    add console logs for debugging

--- ML5/ml5GyroKnn/sketch.js
+++ ML5/ml5GyroKnn/sketch.js
@@ -2,6 +2,7 @@ const knnClassifier = ml5.KNNClassifier();
 let butTrain1, butTrain2, butReset1, butReset2;
 let class1Examples, class2Examples;
 let currentClass;
+let debug = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -28,7 +29,7 @@ function setup() {
   butReset2 = createButton('Reset class 2');
   butReset2.position(19, 79);
   butReset2.touchStarted(function(){
-    trainModel(getOrientation(), '1');
+    trainModel(getOrientation(), '2');
   });
 }
 
@@ -36,13 +37,22 @@ function draw() {
   background(255);
 
   text(currentClass, 19, 0);
+  if(debug){
+    console.log(currentClass);
+  }
 }
 
 function trainModel(features, label){
   knnClassifier.addExample(features, label);
+  if(debug){
+    console.log('training label ' + label);
+  }
 }
 
 function deviceMoved(){
+  if(debug){
+    console.log('device moved')
+  }
   knnClassifier.classify(features, function(err, result) {
     if(err){
       console.log(err);
@@ -73,12 +83,18 @@ function updateCounts() {
 
 // Clear the examples in one label
 function clearLabel(label) {
+  if(debug){
+    console.log('clearing label ' + label);
+  }
   knnClassifier.clearLabel(label);
   updateCounts();
 }
 
 // Clear all the examples in all labels
 function clearAllLabels() {
+  if(debug){
+    console.log('clearing all labels');
+  }
   knnClassifier.clearAllLabels();
   updateCounts();
 }
